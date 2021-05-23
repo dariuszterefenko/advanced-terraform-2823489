a7189408ec0a6fd26304d3c61a53ac42697ad816
Dariusz Terefenko <dariusz.terefenko@alchemyglobalsolutions.com>
2021-05-23 20:17:48 +0100

    set environment tag to staging

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "bucket_name" {
-  default = "red30-tfstate"
+  default = "dt20210523"
 }
 
 # //////////////////////////////
@@ -15,7 +15,7 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-2"
+  region = "eu-west-1"
 }
 
 # //////////////////////////////
@@ -28,7 +28,7 @@ data "aws_iam_user" "terraform" {
 # //////////////////////////////
 # S3 BUCKET
 # //////////////////////////////
-resource "aws_s3_bucket" "red30-tfremotestate" {
+resource "aws_s3_bucket" "dt20210523" {
   bucket = var.bucket_name
   force_destroy = true
   acl = "private"
@@ -57,7 +57,7 @@ EOF
 }
 
 resource "aws_s3_bucket_public_access_block" "red30-tfremotestate" {
-  bucket = aws_s3_bucket.red30-tfremotestate.id
+  bucket = aws_s3_bucket.dt20210523.id
 
   block_public_acls   = true
   block_public_policy = true
--- 03_07_custommodule/modules/nodejs-instance/variables.tf
+++ 03_07_custommodule/modules/nodejs-instance/variables.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
     description = "The ID of the NodeJS AMI to deploy"
-    default = "ami-6685a403"
+    default = "ami-0d5eff06f840b45e9"
 }
 
 variable instance_count {
